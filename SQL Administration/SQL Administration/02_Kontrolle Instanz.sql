2bdfca800349da18ca11cf20d414907db90b2f23
Rauchendraser <andreasr@ppedv.de>
2022-08-08 13:04:17 +0200

    SysytemDBs Wartungsplan

---
+++ SQL Administration/SQL Administration/02_Kontrolle Instanz.sql
@@ -0,0 +1,29 @@
+--Kontrolle des HV-SQL1
+
+6 Gb RAM und 4 / 8 Kerne
+
+--MAX RAM
+--2,1PB ? Korrektur auf 4098 ist sofort gültig
+--Min RAM gilt erst wenn erreicht..
+
+
+
+
+
+--MAXDOP
+--Anzahl der Kerne , die max bei Abfragen beteiligt sind
+--entder 1 Kern oder alle des MAXDOP
+--Regel: MAXDOP = Anzahl der Kerne , aber max 8
+--durch Setup konfigurierte Werte werden später nicht automatisch angepasst, wenn sich die Hardware ändert
+--besonders bei Virtualisierung
+
+
+
+
+--Standardpfade
+--sind ok.. C:\SQLDBs. Änderungen erst nach Neustart des SQL Servers gültig
+
+--te,pdb
+
+--Security
+
